498e3be7f58547b2f631f9f85c392149e3442d43
wenhaus <projectblueboy@gmail.com>
2016-01-11 19:37:07 +0000

    nitrous-backup at Mon Jan 11 19:37:07 UTC 2016

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown (n)
+  while n>0
+    puts "#{n} SECOND(S)!"
+    n-=1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  while n>0
+    sleep(1)
+    puts n
+    n-=1
+  end
 end
